fd0d0840346d827227318a07762ba49851a4d242
ahmedfahmy36 <superbota2000@gmail.com>
2026-07-22 18:02:05 +0300

    add new favicon to public assets , changed title of the project

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,13 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   modules: ['@pinia/nuxt', '@nuxtjs/tailwindcss'],
-  css: ['~/assets/css/main.css']
+  css: ['~/assets/css/main.css'],
+  app: {
+    head: {
+      title: 'T.M.S | Task Management System',
+      meta: [
+        { name: 'description', content: 'A professional, modern task management system.' }
+      ]
+    }
+  }
 })
